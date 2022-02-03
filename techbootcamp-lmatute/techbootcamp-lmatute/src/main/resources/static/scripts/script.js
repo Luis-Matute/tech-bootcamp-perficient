db09cb28c184784619bee2f4b68dc966abaf747f
Luis Matute <luis.matute@perficient.com>
2022-02-03 14:20:26 -0600

    Different display for blogs
    Modified the structure in which blogs are displayed

--- techbootcamp-lmatute/techbootcamp-lmatute/src/main/resources/static/scripts/script.js
+++ techbootcamp-lmatute/techbootcamp-lmatute/src/main/resources/static/scripts/script.js
@@ -11,29 +11,22 @@ function loadBlogs() {
 
         success: function(response) {
             console.log(response);
-            var table = $("<table><tr><th>Blog Post Data</th></tr>");
+            var table = $("<div class='blogs'>");
 
             response.forEach(element => {
-                table.append(
-                `<tr>
-                    <td>
-                        Data:
-                    </td>
-
-                    <td>
-                        ${element["id"]}
-                    </td>
-
-                    <td>
-                        ${element["title"]}
-                    </td>
-                </tr>`);
+                
+                table.append(`
+                    <div class='box'>
+                        <h3>${element["title"]}</h3>
+                        <h5>${element["content"]}</h5>
+                    </div>
+                `);
             });
             
            
-            table.append("</table>");
+            table.append("</div>");
 
-            $("#message").html(table);
+            $("#blogs-container").html(table);
         },
 
         error: function(response) {
@@ -42,7 +35,7 @@ function loadBlogs() {
     });
 };
 
-// $(document).ready(function() {
+// $(document).ready(function() {   
 //     $.ajax({
 //         url: "http://localhost:8080/allBlogPosts",
 //         jsonp: "callback",
